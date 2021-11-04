8c7d6c56f2bc6a1f761ce856f0ea882c4dea333e
alexp <alexpetrescu183@gmail.com>
2021-11-04 22:23:59 +0200

    added program

---
+++ gdsc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace gdsc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool logging=true;
+            for (int i = 9; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+                if(i==7)
+                {
+                    if(logging==true)
+                    {
+                        Console.WriteLine("We found 7!");
+                    }
+                   
+
+                }
+            }
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (false);
+        }
+       
+       /* void Print()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello {name}");
+        } */
+    }
+}
